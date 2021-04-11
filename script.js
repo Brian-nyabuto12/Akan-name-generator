9185937ea422235a2983632971a12899e1176db8
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-04-11 12:49:15 +0300

    add fuction that will calculate and checks gender

--- script.js
+++ script.js
@@ -8,7 +8,19 @@ var femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
 
 var day= function(){
+    century = parseInt(document.getElementById("cenuty").value);
     year = parseInt(document.getElementById("year").value);
     month = parseInt(document.getElementById("year").value);
     date = parseInt(document.getElementById("date").value);
+}
+
+function calculate(){
+    day();
+    dayOfTheWeek = ( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + DD ) % 7
+    return(math.floor(dayOfTheWeek));
+    console.log(dayOfTheWeek);
+}
+var result = function(){
+    dayOfTheWeek = calculate();
+    checkgender();
 }
