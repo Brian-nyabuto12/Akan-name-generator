8bc6cc6c52121c61335dc3a997fe0247b72bd051
brian-nyabuto12 <brian.nyabuto@student.moringaschool.com>
2021-04-11 12:27:56 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var year;
+var month;
+var date;
+var dayOfTheWeek;
+var maleNames= ["kwasi","Kwadwo","Kwaku","Yaw","Kofi","kwame"];
+var femaleNames= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+
+
+var day= function(){
+    year = parseInt(document.getElementById("year").value);
+    month = parseInt(document.getElementById("year").value);
+    date = parseInt(document.getElementById("date").value);
+}
